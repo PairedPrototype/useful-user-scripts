50aa6b65f3c371096f6eceb9cdec8e2cc4095f7a
PairedPrototype <pairedprototype@hotmail.com>
2021-10-11 00:34:20 +0100

    `take-me-to-amazon-smile-always` updated with update and download URLs

--- src/scripts/amazon/take-me-to-amazon-smile-always.js
+++ src/scripts/amazon/take-me-to-amazon-smile-always.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name        Take me to Amazon Smile always!
-// @namespace   https://github.com/PairedPrototype/useful-user-scripts
-// @version     1.0.0
 // @description Redirects you to the Amazon Smile domain if you find yourself on the normal site. I'm not sure why Amazon can't just support charities anyway, but here we are, needing a more convenient way to help us do that.
-// @author      PairedPrototype
+// @version     1.0.0
+// @updateURL   https://raw.githubusercontent.com/PairedPrototype/useful-user-scripts/master/src/scripts/amazon/take-me-to-amazon-smile-always.js
+// @downloadURL https://raw.githubusercontent.com/PairedPrototype/useful-user-scripts/master/src/scripts/amazon/take-me-to-amazon-smile-always.js
+// @namespace   https://github.com/PairedPrototype/useful-user-scripts
 // @include     /.*:\/\/(www\.|)amazon\.(com|co.uk|de)\/.*/
 // @exclude     *://smile.amazon.*/*
-// @grant       none
+// @author      PairedPrototype
 // @supportURL  https://github.com/PairedPrototype/useful-user-scripts/issues
+// @grant       none
 // ==/UserScript==
 
 (() => {
