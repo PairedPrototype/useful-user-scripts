027fb7dd3116e47a3e52fac72f3f881784538e7b
PairedPrototype <pairedprototype@hotmail.com>
2021-10-11 00:51:12 +0100

    Added Amazon icon

--- src/scripts/amazon/take-me-to-amazon-smile-always.js
+++ src/scripts/amazon/take-me-to-amazon-smile-always.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Take me to Amazon Smile always!
 // @description Redirects you to the Amazon Smile domain if you find yourself on the normal site. I'm not sure why Amazon can't just support charities anyway, but here we are, needing a more convenient way to help us do that.
-// @version     1.0.0
+// @version     1.0.1
 // @updateURL   https://raw.githubusercontent.com/PairedPrototype/useful-user-scripts/master/src/scripts/amazon/take-me-to-amazon-smile-always.js
 // @downloadURL https://raw.githubusercontent.com/PairedPrototype/useful-user-scripts/master/src/scripts/amazon/take-me-to-amazon-smile-always.js
 // @namespace   https://github.com/PairedPrototype/useful-user-scripts
@@ -9,6 +9,7 @@
 // @exclude     *://smile.amazon.*/*
 // @author      PairedPrototype
 // @supportURL  https://github.com/PairedPrototype/useful-user-scripts/issues
+// @icon        https://www.amazon.com/favicon.ico
 // @grant       none
 // ==/UserScript==
 
