7a192aeb77ee81996ead4b28e2fc3ea92e6101c8
PairedPrototype <pairedprototype@hotmail.com>
2021-11-01 20:07:49 +0000

    Added a template script

---
+++ src/script-template.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Name
+// @description Does stuff
+// @version     1.0.0
+// @updateURL   https://raw.githubusercontent.com/PurplProto/useful-user-scripts/master/src/scripts/
+// @downloadURL https://raw.githubusercontent.com/PurplProto/useful-user-scripts/master/src/scripts/
+// @namespace   https://github.com/PurplProto/useful-user-scripts
+// @include     /.*:\/\/(www\.|)aWebSite\.(com|co.uk)\/.*/
+// @exclude     *://somthing.maybe.*/*
+// @author      PurplProto
+// @supportURL  https://github.com/PurplProto/useful-user-scripts/issues
+// @icon        https://www.example.com/favicon.ico
+// @grant       none
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  // Do stuff
+})();
